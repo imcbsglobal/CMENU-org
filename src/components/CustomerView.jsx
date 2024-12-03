e3bdfbdd6a169488c26a2161c74cf8b15f452046
amalkrishna0123 <amalikka0@gmail.com>
2024-12-03 16:56:04 +0530

    GST Added Colour Changed

--- src/components/CustomerView.jsx
+++ src/components/CustomerView.jsx
@@ -411,7 +411,7 @@ useEffect(() => {
               <BannerSection banners={banners} />
             </div>
             {/* GST Display */}
-            <div className="mb-2 px-2 font-semibold text-[#fff] flex justify-start items-center gap-2">{gstTitle}<span><HiReceiptTax/></span></div>
+            <div className="mb-2 px-2 font-semibold flex justify-start items-center gap-2 ItemText" style={{color : fontColor}}>{gstTitle}<span><HiReceiptTax/></span></div>
             {/* Categories Section */}
             <div className="flex justify-start items-start mb-5 px-2">
               <div className="relative flex justify-start items-center">
